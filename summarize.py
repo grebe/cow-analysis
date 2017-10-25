549e663169efda55ced71433aea841d39636aa16
Paul Rigge <rigge@berkeley.edu>
2017-10-25 14:27:49 -0700

    Add some command line argument parsing and frequency estimation

--- summarize.py
+++ summarize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import math
 import numpy as np
 import scipy.signal as sig
@@ -30,6 +31,13 @@ def countCompleteCycles(vec):
     zero_crossings = np.where(np.diff(signs))[0]
     return len(zero_crossings)
 
+def estimateFrequency(vec, ts=1.0/20.0e6):
+    vec_hat = np.abs(np.fft.fft(vec))
+    n = vec.size
+    freqs = np.fft.fftfreq(n, d=ts)
+    idx = np.argmax(vec_hat)
+    return freqs[idx]
+
 def generateTone(SNRdB):
     sigma = 10 ** (-SNRdB / 10.0)
     t = np.arange(0, 16000000)
@@ -42,14 +50,14 @@ def generateTone(SNRdB):
     signalShort[1::2] = signalShortImag
     return signalShort
 
-def completeCyclesAddToFile(vec, vecname, fcounts):
+def addToFile(fun, vec, vecname, fcounts):
     # filt = brickwall(100.0e3, 270.0e3)
     # vec  = np.convolve(vec, filt)
-    cycles = countCompleteCycles(vec)
+    result = fun(vec)
     with open(fcounts, 'a') as histfile:
         histfile.write(vecname)
         histfile.write("\t\t")
-        histfile.write(str(cycles))
+        histfile.write(str(result))
         histfile.write("\n")
 
 def rreplace(s, old, new, occurrence):
@@ -59,11 +67,41 @@ def rreplace(s, old, new, occurrence):
 def fnameSuffix(fname, suffix):
     return rreplace(fname, ".", suffix+".", 1)
 
+def getArgs():
+    parser = argparse.ArgumentParser(description='Generate summary stastistics for radio traces')
+    parser.add_argument('--input', metavar='N', nargs=1, required=True,
+                       help='the input file (binary)')
+    parser.add_argument('--output', nargs=1, required=True,
+                       help='the output file')
+    parser.add_argument('--countCycles', action='store_true',
+                        help='')
+    parser.add_argument('--estimateFrequency', action='store_true',
+                        help='')
+    parser.add_argument('--nSplits', type=int, default=1,
+                        help='')
+    parser.add_argument('--fs', type=float, nargs=1, default=20.0e6,
+                        help='')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    vec = readADIDump(sys.argv[1])
-    nSplits = int(sys.argv[2])
+    args = getArgs()
+    inputFile = args.input[0]
+    outputFile = args.output[0]
+    nSplits = args.nSplits
+    fs = args.fs
+    ts = 1.0 / fs
+    if args.countCycles:
+        fun = lambda x: countCompleteCycles(x)
+    elif args.estimateFrequency:
+        fun = lambda x: estimateFrequency(x, ts)
+    else:
+        fun = lambda x: None
+    print(inputFile)
+    vec = readADIDump(inputFile)
     vec = splitADIDump(vec, nSplits)
+    
     for i in range(len(vec)):
         v = vec[i]
-        completeCyclesAddToFile(v, fnameSuffix(sys.argv[1], "_" + str(i)), sys.argv[3])
+        addToFile(fun, v, fnameSuffix(inputFile, "_" + str(i)), outputFile)
 
